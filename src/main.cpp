3141163714303cac1d3812717e6310bd7d8b0ecf
aravindkarav2026-arch <aravindkarav2026@gmail.com>
2026-08-23 18:35:29 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,63 +1,36 @@
 #include <Arduino.h>
-#include <WiFi.h>
 #include <Adafruit_NeoPixel.h>
 
-const char* ssid = "YOUR_WIFI_NAME";
-const char* password = "YOUR_WIFI_PASSWORD";
-
-// Waveshare ESP32-C6 onboard RGB LED is on GPIO 8
 #define RGB_PIN 8
 #define NUM_PIXELS 1
 
 Adafruit_NeoPixel rgb(NUM_PIXELS, RGB_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  Serial.begin(115200);
-
   rgb.begin();
-  rgb.setBrightness(30); // Set low brightness (0-255) to avoid harsh light
+  rgb.setBrightness(40); // Brightness scale 0-255
   rgb.clear();
   rgb.show();
-
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting to Wi-Fi");
-
-  // Blink BLUE while connecting to Wi-Fi
-  while (WiFi.status() != WL_CONNECTED) {
-    rgb.setPixelColor(0, rgb.Color(0, 0, 255)); // Blue ON
-    rgb.show();
-    delay(250);
-
-    rgb.setPixelColor(0, rgb.Color(0, 0, 0));   // OFF
-    rgb.show();
-    delay(250);
-
-    Serial.print(".");
-  }
-
-  // Solid GREEN once Wi-Fi is connected
-  rgb.setPixelColor(0, rgb.Color(0, 255, 0)); // Green ON
-  rgb.show();
-
-  Serial.println("\nConnected to Wi-Fi!");
-  Serial.print("IP Address: ");
-  Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  if (WiFi.status() != WL_CONNECTED) {
-    // Flash RED if Wi-Fi disconnects
-    rgb.setPixelColor(0, rgb.Color(255, 0, 0)); // Red
-    rgb.show();
-    delay(100);
-    rgb.setPixelColor(0, rgb.Color(0, 0, 0));
-    rgb.show();
-    delay(100);
-    WiFi.reconnect();
-  } else {
-    // Keep solid GREEN when healthy
-    rgb.setPixelColor(0, rgb.Color(0, 255, 0));
-    rgb.show();
-    delay(1000);
-  }
+  // Red
+  rgb.setPixelColor(0, rgb.Color(255, 0, 0));
+  rgb.show();
+  delay(500);
+
+  // Green
+  rgb.setPixelColor(0, rgb.Color(0, 255, 0));
+  rgb.show();
+  delay(500);
+
+  // Blue
+  rgb.setPixelColor(0, rgb.Color(0, 0, 255));
+  rgb.show();
+  delay(500);
+
+  // Off
+  rgb.setPixelColor(0, rgb.Color(0, 0, 0));
+  rgb.show();
+  delay(500);
 }
